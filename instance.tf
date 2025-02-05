e9b5356751c0c5cc87c449942cbbcc8594c16c93
pavan <pavan@gmail.com>
2025-02-05 11:48:34 +0530

    A

---
+++ instance.tf
@@ -0,0 +1,22 @@
+# variable
+
+# provider
+provider "aws" {
+  
+  region = "us-east-1"
+}
+
+
+
+
+# Resources
+resource "aws_instance" "pavaninstance" {
+    ami = "ami-0c614dee691cbbf37"
+    instance_type = "t2.micro"
+    key_name = "pavankey"
+  
+}
+
+
+# output
+output "pavaninstanceID" {value=aws_instance.pavaninstance.id} 
